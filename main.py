b15a16b31252b4e4e63f75b51040057b93412d29
amicuswat <amicuswat@gmail.com>
2022-10-26 16:12:23 +0300

    add has_digits and is_very_long functions

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 
 
-def main():
-    password = input('Введите пароль:')
-
-    if len(password) > 12:
-        print('Длинный')
-    else:
-        print('Короткий')
-
+def has_digits(password):
     found_digits = False
     for letter in password:
         if letter.isdigit():
             found_digits = True
             break
 
-    if found_digits:
-        print('Есть цифры')
-    else:
-        print('Нет цифр')
+    return found_digits
+
+
+def is_very_long(password):
+    return len(password) > 12
+
+
+def main():
+    password = input('Введите пароль:')
+
+    print(has_digits(password))
+    print(is_very_long(password))
 
 
 if __name__ == "__main__":
